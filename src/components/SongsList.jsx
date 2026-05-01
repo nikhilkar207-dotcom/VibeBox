bfac28dde36c5ad1ac49f6552a1917048e7a35cf
nikhilkar207-dotcom <nikhilkar207@gmail.com>
2026-05-02 05:17:50 +0530

    Update SongsList.jsx

--- src/components/SongsList.jsx
+++ src/components/SongsList.jsx
@@ -10,12 +10,16 @@ function SongsList() {
 
     const search = useSong(state => state.search);
 
-    const filteredSongs =
-        search.trim() === ""
-            ? songs
-            : songs.filter(song =>
-                song.song_name.toLowerCase().includes(search.toLowerCase())
-            );
+    const filteredSongs = songs.filter(song =>
+        song.song_name.toLowerCase().includes(search.toLowerCase())
+    );
+
+    const filteredHindiSongs = songs.filter(song => song.language === "hindi");
+    const filteredHaryanviSongs = songs.filter(song => song.language === "haryanvi");
+    const filteredPunjabiSongs = songs.filter(song => song.language === "punjabi");
+    const filteredSadRomanticSongs = songs.filter(song => song.moods?.includes("sad") &&
+        song.moods?.includes("romantic"));
+    const filteredRomanticSongs = songs.filter(song => song.moods?.includes("romantic") &&  (song.moods?.includes("happy") || song.moods?.includes("chill") || songs.moods?.includes("happy")));
 
     function playSong(song) {
         const index = songs.findIndex(s => s.id === song.id);
@@ -26,10 +30,41 @@ function SongsList() {
     return (
         <div className="songs-list">
             <section>
-                <h3>For you</h3>
-
+                {search.trim() !== "" ? (
+                    <h3>For you</h3>
+                ) : (
+                    <></>
+                )}
                 <ul className="songs for-you">
-                    {filteredSongs && filteredSongs.map((song, index) => (
+                    {search.trim() !== "" ? (
+                        filteredSongs && filteredSongs.map((song, index) => (
+                            <li
+                                onClick={() => playSong(song)}
+                                key={index}
+                                className="bg"
+                                style={{
+                                    backgroundImage: `url(${song.cover_url})`,
+                                    backgroundSize: "cover",
+                                    backgroundPosition: "center",
+                                    backgroundRepeat: "no-repeat",
+                                }}
+                            >
+                                <div className="overlay">
+                                    <p>{song.song_name}</p>
+                                </div>
+                            </li>
+                        ))
+                    ) : (
+                        <></>
+                    )}
+                </ul>
+            </section>
+
+            <section>
+                <h3>Hindi Songs</h3>
+
+                <ul className="songs hindi-songs">
+                    {filteredHindiSongs && filteredHindiSongs.map((song, index) => (
                         <li
                             onClick={() => playSong(song)}
                             key={index}
@@ -38,12 +73,93 @@ function SongsList() {
                                 backgroundImage: `url(${song.cover_url})`,
                                 backgroundSize: "cover",
                                 backgroundPosition: "center",
-                                backgroundRepeat: "no-repeat"
+                                backgroundRepeat: "no-repeat",
                             }}
                         ><div className="overlay"><p>{song.song_name}</p></div></li>
                     ))}
                 </ul>
             </section>
+
+            <section>
+                <h3>Haryanvi Songs</h3>
+
+                <ul className="songs haryanvi-songs">
+                    {filteredHaryanviSongs && filteredHaryanviSongs.map((song, index) => (
+                        <li
+                            onClick={() => playSong(song)}
+                            key={index}
+                            className="bg"
+                            style={{
+                                backgroundImage: `url(${song.cover_url})`,
+                                backgroundSize: "cover",
+                                backgroundPosition: "center",
+                                backgroundRepeat: "no-repeat",
+                            }}
+                        ><div className="overlay"><p>{song.song_name}</p></div></li>
+                    ))}
+                </ul>
+            </section>
+
+            <section>
+                <h3>Punjabi Songs</h3>
+
+                <ul className="songs punjabi-songs">
+                    {filteredPunjabiSongs && filteredPunjabiSongs.map((song, index) => (
+                        <li
+                            onClick={() => playSong(song)}
+                            key={index}
+                            className="bg"
+                            style={{
+                                backgroundImage: `url(${song.cover_url})`,
+                                backgroundSize: "cover",
+                                backgroundPosition: "center",
+                                backgroundRepeat: "no-repeat",
+                            }}
+                        ><div className="overlay"><p>{song.song_name}</p></div></li>
+                    ))}
+                </ul>
+            </section>
+
+            <section>
+                <h3>Romantic Sad Songs</h3>
+
+                <ul className="songs romantic-sad-songs">
+                    {filteredSadRomanticSongs && filteredSadRomanticSongs.map((song, index) => (
+                        <li
+                            onClick={() => playSong(song)}
+                            key={index}
+                            className="bg"
+                            style={{
+                                backgroundImage: `url(${song.cover_url})`,
+                                backgroundSize: "cover",
+                                backgroundPosition: "center",
+                                backgroundRepeat: "no-repeat",
+                            }}
+                        ><div className="overlay"><p>{song.song_name}</p></div></li>
+                    ))}
+                </ul>
+            </section>
+
+            <section>
+                <h3>Romantic Songs</h3>
+
+                <ul className="songs romantic-songs">
+                    {filteredRomanticSongs && filteredRomanticSongs.map((song, index) => (
+                        <li
+                            onClick={() => playSong(song)}
+                            key={index}
+                            className="bg"
+                            style={{
+                                backgroundImage: `url(${song.cover_url})`,
+                                backgroundSize: "cover",
+                                backgroundPosition: "center",
+                                backgroundRepeat: "no-repeat",
+                            }}
+                        ><div className="overlay"><p>{song.song_name}</p></div></li>
+                    ))}
+                </ul>
+            </section>
+
         </div>
     );
 }
