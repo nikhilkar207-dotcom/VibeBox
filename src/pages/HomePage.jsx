e4a7949c82a7944ce45356e641a63bf6778cebd0
nikhilkar207-dotcom <nikhilkar207@gmail.com>
2026-05-02 04:31:10 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,27 @@
 import { React } from "react";
 import MusicPlayer from "../components/MusicPlayer";
+import SongsList from "../components/SongsList";
 import "../styles/HomeStyle.css";
 import { FiSearch } from "react-icons/fi";
 
+import { getSongs, useSong} from "../musicStore";
+
 function HomePage() {
+
+    const search = useSong(state => state.search);
+    const setSearch = useSong(state => state.setSearch);
+
+    getSongs();
     return (
         <div>
             <header>
                 <h2>VibeBox</h2>
                 <div className="search-container">
                    <FiSearch />
-                    <input type="text" placeholder="Search" />
+                    <input type="text" value={search} onChange = {(e) => setSearch(e.target.value)} placeholder="Search" />
                 </div>
             </header>
+            <SongsList />
             <MusicPlayer />
         </div>
     );
