2489ef134754d422a076fdf23ce10e6f70ae8ebe
nikhilkar207-dotcom <nikhilkar207@gmail.com>
2026-05-02 01:57:27 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,18 @@
 import { React } from "react";
 import MusicPlayer from "../components/MusicPlayer";
+import "../styles/HomeStyle.css";
+import { FiSearch } from "react-icons/fi";
 
 function HomePage() {
     return (
         <div>
+            <header>
+                <h2>VibeBox</h2>
+                <div className="search-container">
+                   <FiSearch />
+                    <input type="text" placeholder="Search" />
+                </div>
+            </header>
             <MusicPlayer />
         </div>
     );
