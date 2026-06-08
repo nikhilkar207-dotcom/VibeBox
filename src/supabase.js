99a3551243a19d48f2317a1e3299f0f07be24aa3
nikhilkar207-dotcom <nikhilkar207@gmail.com>
2026-06-09 00:15:41 +0530

    Delete src directory

--- src/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://lixbnupundwtlcbumzdy.supabase.co";
-const supabaseKey = "sb_publishable_y8WytrQtRlZHQvef6wLBSg_ZhaIBQOn";
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
